06275e83220afd8c1a6618f5b31a8e16e4c91a19
JuniorBaymax <cooladitya953@gmail.com>
2021-03-07 21:56:24 +0530

    Simplified App and removed unnecessary codes and files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHf3wfM_MxUD_j7f1zvMB0uodX6iEvYic',
+  authDomain: 'slack-clone-b3113.firebaseapp.com',
+  projectId: 'slack-clone-b3113',
+  storageBucket: 'slack-clone-b3113.appspot.com',
+  messagingSenderId: '925261091950',
+  appId: '1:925261091950:web:4631232bebad526b6e827f',
+  measurementId: 'G-27LLCRTCPZ',
+};

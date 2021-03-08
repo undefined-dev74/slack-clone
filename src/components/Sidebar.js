d31187d8b92da75c9fb2c5e06f9e1f3f279ebecc
JuniorBaymax <cooladitya953@gmail.com>
2021-03-09 00:52:00 +0530

    Customize Sidebar added Add channel option

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -55,6 +55,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   min-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { db } from '../firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 const SidebarOption = ({ Icon, title, addChannelOption }) => {
+  const [channel, loading, error] = useCollection(db.collection('rooms'));
+
+  const addChannel = () => {
+    const channelName = prompt('Please enter the channel Name');
+
+    if (channelName) {
+      db.collection('rooms').add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {};
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
         <h3>{title}</h3>
